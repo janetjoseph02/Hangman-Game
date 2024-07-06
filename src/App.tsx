4c61afd29964db9ab4c6cb7a508f4ccbc52011d7
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-07 01:11:25 +0500

    update 1 file and create 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { generate, count } from "random-words";
+import HangmanDrawing from './Components/HangmanDrawing/HangmanDrawing';
+import HangmanWord from './Components/HangmanWord/HangmanWord';
+import Keyboard from './Components/Keyboard/Keyboard';
+
 
 function App() {
 
@@ -14,9 +18,14 @@ function App() {
 
   return (
     <>
-      <h1 className="text-pink-600">
-        Hello world!
-      </h1>
+      <div className="main flex flex-col gap-8 items-center max-w-[800px] m-10 bg-purple-200">
+
+        <h1 className="message text-2xl font-semibold text-center">Lose/Win Message</h1>
+        <HangmanDrawing/>
+        <HangmanWord/>
+        <Keyboard/>
+
+      </div>
     </>
   )
 }
---
+++ src/Components/HangmanDrawing/HangmanDrawing.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const HEAD = <div className=' w-[50px] h-[50px] bg-transparent border-[5px] border-red-700 z-10 rounded-full absolute top-[45px] right-[-18px]'/>
+const BODY = <div className=' w-[5px] h-[60px] bg-red-700 z-10  absolute top-[95px] right-[5px] '/>
+const RIGHT_ARM = <div className=' w-[5px] h-[30px] bg-red-700 z-10  absolute top-[95px] right-[15px] rotate-45 rounded-b-[30px]'/>
+const LEFT_ARM = <div className=' w-[5px] h-[30px] bg-red-700 z-10  absolute top-[95px] right-[-5px] rotate-[-45deg] rounded-b-[30px]'/>
+const RIGHT_LEG = <div className=' w-[5px] h-[40px] bg-red-700 z-10  absolute top-[145px] right-[20px] rotate-45 rounded-b-[30px]'/>
+const LEFT_LEG = <div className=' w-[5px] h-[40px] bg-red-700 z-10  absolute top-[145px] right-[-10px] rotate-[-45deg] rounded-b-[30px]'/>
+
+
+
+
+const HangmanDrawing = () => {
+  return (
+    <div className="main relative">
+
+        {HEAD}
+        {BODY}
+        {RIGHT_ARM}
+        {LEFT_ARM}
+        {RIGHT_LEG}
+        {LEFT_LEG}
+        <div className='topLeft h-[50px] w-[6px] bg-blue-900 absolute top-0 right-[5px] rounded-b-3xl '/>
+        <div className='top h-[10px] w-[125px] bg-blue-900 rounded-3xl rounded-l-none ml-[120px] '/>
+        <div className='mid w-[10px] h-[250px] bg-blue-900  ml-[120px] '/>
+        <div className="bottom h-[10px] w-[250px] bg-blue-900 rounded-3xl "/>
+
+    </div>
+  )
+}
+
+export default HangmanDrawing
