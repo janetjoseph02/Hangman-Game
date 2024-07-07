433490b2ffb462644dec41b99e2209a57d96db30
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-07 23:46:12 +0500

    update App.tsx and Keyboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,24 @@ import Keyboard from './Components/Keyboard/Keyboard';
 function App() {
 
 
-  const [wordToGuess, setWordToGuess] = useState(generate({ minLength: 3 }));
+  const [wordToGuess, setWordToGuess] = useState(generate({ minLength: 4 }));
   // const [wordToGuess, setWordToGuess] = useState("SARMAD");
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   const incorrectGuesses = guessedLetters.filter(item => !wordToGuess.includes(item))
 
+  const isLoser = incorrectGuesses.length >= 6;
+  const isWinner = wordToGuess
+                    .toString()
+                    .split("")
+                    .every(letter => guessedLetters.includes(letter))
+
   console.log(wordToGuess)
 
-  const addGuessedLetter = (letter:string) => {
-    if(guessedLetters.includes(letter)) return
+  const addGuessedLetter = (letter: string) => {
+    if (guessedLetters.includes(letter)) return
 
     setGuessedLetters(currentLetters => [...currentLetters, letter])
-    
+
   }
 
 
@@ -52,13 +58,17 @@ function App() {
     <>
       <div className="main flex flex-col gap-8 items-center max-w-[800px] m-10 h-auto w-screen">
 
-        <h1 className="message text-2xl font-semibold text-center">Lose/Win Message</h1>
+        <h1 className={`message text-2xl font-semibold text-center cursor-pointer hover:opacity-80 ${isWinner ? "text-green-600" : "text-red-600"}`} onClick={() => {window.location.reload()}}>
+          {isLoser && "You Lose. Click to Try Again. 😞"}
+          {isWinner && "You Won 👏 Click to Play again"}
+        </h1>
+
         <HangmanDrawing numberOfGuess={incorrectGuesses.length} />
         <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
-        <Keyboard 
-          activeLetter = {guessedLetters.filter(letter => wordToGuess.includes(letter))}
-          inactiveLetter = {incorrectGuesses}
-          addGuessedLetter= {addGuessedLetter}
+        <Keyboard
+          activeLetter={guessedLetters.filter(letter => wordToGuess.includes(letter))}
+          inactiveLetter={incorrectGuesses}
+          addGuessedLetter={addGuessedLetter}
         />
 
       </div>
--- src/Components/Keyboard/Keyboard.tsx
+++ src/Components/Keyboard/Keyboard.tsx
@@ -1,24 +1,34 @@
-import React, { useEffect } from 'react'
-import styles from './Keyboard.module.css'
+import React from 'react';
+import styles from './Keyboard.module.css';
 
 const KEY: string[] = Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
-// console.log(KEY)
-type keyboardProps = {
-    activeLetter: string[],
-    inactiveLetter: string[],
-    guessedLetters: (letter: string) => void,
-}
 
-const Keyboard = ({ activeLetter, inactiveLetter, guessedLetters }: keyboardProps) => {
-    return (
-        <div className='flex flex-wrap items-center justify-center gap-2'>
-            {KEY.map((key, index) => (
-                <button className={`w-12 h-14 border border-black rounded-2xl hover:bg-blue-900 hover:text-white transition select-none active:bg-blue-700 ${styles.btn} ${styles.active}`} key={index}>
-                    {key.toUpperCase()}
-                </button>
-            ))}
-        </div>
-    )
-}
+type KeyboardProps = {
+  activeLetter: string[],
+  inactiveLetter: string[],
+  addGuessedLetter: (letter: string) => void,
+};
 
-export default Keyboard
+const Keyboard: React.FC<KeyboardProps> = ({ activeLetter, inactiveLetter, addGuessedLetter }) => {
+  return (
+    <div className='flex flex-wrap items-center justify-center gap-2'>
+      {KEY.map(key => {
+        const isActive = activeLetter.includes(key);
+        const isInActive = inactiveLetter.includes(key);
+
+        return (
+          <button
+            onClick={() => { addGuessedLetter(key) }}
+            className={`w-12 h-14 border border-black rounded-2xl hover:bg-blue-900 hover:text-white transition select-none active:bg-blue-700 ${styles.btn} ${isActive ? 'bg-blue-900 text-white active:bg-blue-900' : ''} ${isInActive ? ' bg-red-600 text-white opacity-50 hover:bg-red-600 active:bg-red-600' : ''}`}
+            key={key}
+            disabled={isActive || isInActive}
+          >
+            {key.toUpperCase()}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Keyboard;
