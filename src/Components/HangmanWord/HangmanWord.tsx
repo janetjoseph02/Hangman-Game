70c617146a9ab53799abf638566df1d2a3cf6b1e
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-07 08:42:22 +0500

    update HangmanWord.tsx

--- src/Components/HangmanWord/HangmanWord.tsx
+++ src/Components/HangmanWord/HangmanWord.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const HangmanWord = () => {
-  return (
-    <div>HangmanWord</div>
-  )
+type HangmanWordProps = {
+    guessedLetters: string[],
+    wordToGuess: string
+}
+
+const HangmanWord = ({ guessedLetters, wordToGuess }: HangmanWordProps) => {
+
+    return (
+        <div className='flex items-center gap-6 uppercase text-[4rem] font-bold '>
+            {wordToGuess.split("").map((letter, index) => (
+                <span className=' border-b-4 border-black w-16 flex items-center justify-center' key={index}>
+                    <span className={`${guessedLetters.includes(letter) ? "visible" : "invisible"}`}>
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    )
 }
 
 export default HangmanWord
