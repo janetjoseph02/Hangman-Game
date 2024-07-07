1349fb37e841b540dedc5bcb4320d27607c0eb2b
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-07 08:42:11 +0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,18 +12,50 @@ function App() {
 
 
   const [wordToGuess, setWordToGuess] = useState(generate({ minLength: 3 }));
-  const [guessedLetters, setuessedLetters] = useState<string[]>([]);
+  // const [wordToGuess, setWordToGuess] = useState("SARMAD");
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const incorrectGuesses = guessedLetters.filter(item => !wordToGuess.includes(item))
 
   console.log(wordToGuess)
 
+  const addGuessedLetter = (letter:string) => {
+    if(guessedLetters.includes(letter)) return
+
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+    
+  }
+
+
+  useEffect(() => {
+
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/[a-z]/i)) {
+        return null;
+      }
+
+      else {
+        e.preventDefault();
+        addGuessedLetter(key)
+      }
+    }
+
+    document.addEventListener('keypress', handler);
+
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [guessedLetters])
+
   return (
     <>
-      <div className="main flex flex-col gap-8 items-center max-w-[800px] m-10 bg-purple-200">
+      <div className="main flex flex-col gap-8 items-center max-w-[800px] m-10 h-auto w-screen">
 
         <h1 className="message text-2xl font-semibold text-center">Lose/Win Message</h1>
-        <HangmanDrawing/>
-        <HangmanWord/>
-        <Keyboard/>
+        <HangmanDrawing numberOfGuess={incorrectGuesses.length} />
+        <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+        <Keyboard />
 
       </div>
     </>
