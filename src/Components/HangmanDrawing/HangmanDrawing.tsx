6c1da2a19772fbbd1e63cbf79910f2da1a55902c
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-07 08:42:15 +0500

    update HangmanDrawing.tsx

--- src/Components/HangmanDrawing/HangmanDrawing.tsx
+++ src/Components/HangmanDrawing/HangmanDrawing.tsx
@@ -10,17 +10,22 @@ const LEFT_LEG = <div className=' w-[5px] h-[40px] bg-red-700 z-10  absolute top
 
 
 
+type HangManDrawingProps = {
+  numberOfGuess: number;  
+}
 
-const HangmanDrawing = () => {
+const HangmanDrawing = ({numberOfGuess} : HangManDrawingProps) => {
+
   return (
     <div className="main relative">
 
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        {RIGHT_LEG}
-        {LEFT_LEG}
+        {numberOfGuess >= 1 ? (HEAD) : null}
+        {numberOfGuess >= 2 ? (BODY) : null}
+        {numberOfGuess >= 3 ? (RIGHT_ARM) : null}
+        {numberOfGuess >= 4 ? (LEFT_ARM) : null}
+        {numberOfGuess >= 5 ? (RIGHT_LEG) : null}
+        {numberOfGuess >= 6 ? (LEFT_LEG) : null}
+       
         <div className='topLeft h-[50px] w-[6px] bg-blue-900 absolute top-0 right-[5px] rounded-b-3xl '/>
         <div className='top h-[10px] w-[125px] bg-blue-900 rounded-3xl rounded-l-none ml-[120px] '/>
         <div className='mid w-[10px] h-[250px] bg-blue-900  ml-[120px] '/>
